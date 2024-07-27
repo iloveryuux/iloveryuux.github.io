759819b3ffd9f846ed9fa4030aaba470aece9efc
iloveryuux <martiin17jr@gmail.com>
2024-07-27 19:17:51 +0200

    ✨ feat: add seo tags

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 import { defineConfig } from 'astro/config'
+
 import tailwind from '@astrojs/tailwind'
+import sitemap from '@astrojs/sitemap'
+import robots from 'astro-robots-txt'
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [tailwind()],
+	integrations: [tailwind(), sitemap(), robots()],
 })
---
+++ src/types.d.ts
@@ -0,0 +1,11 @@
+export interface MetaData {
+	title?: string
+	description?: string
+	og?: {
+		title: string
+		type: string
+		description: string
+		image: string
+		alt: string
+	}
+}

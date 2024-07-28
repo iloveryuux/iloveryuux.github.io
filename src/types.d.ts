d23f003554c91c62513c7f2491851797c5387c01
iloveryuux <martiin17jr@gmail.com>
2024-07-28 21:59:14 +0200

    ✨ feat: timeline section

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,11 +1,11 @@
 export interface MetaData {
-	title?: string
-	description?: string
-	og?: {
-		title: string
-		type: string
-		description: string
-		image: string
-		alt: string
-	}
+  title?: string
+  description?: string
+  og?: {
+    title: string
+    type: string
+    description: string
+    image: string
+    alt: string
+  }
 }
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,29 +1,29 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	darkMode: 'selector',
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {
-			colors: {
-				'background-color': 'hsl(var(--background-color))',
-				'primary-color': 'hsl(var(--text-color))',
-				'secondary-color': 'hsl(var(--footer-color))',
-				'underline-color': 'hsl(var(--underline-color))',
-				'underline-hover-color': 'hsl(var(--footer-hover-color))',
-				'card-color': 'hsl(var(--card-color))',
-				'card-hover-color': 'hsl(var(--card-hover-color))',
-				'underline-color1': 'hsl(var(--underline-color1) / 0.2)',
-				'underline-color2': 'hsl(var(--underline-color2) / 0.2)',
-				'underline-color3': 'hsl(var(--underline-color3) / 0.2)',
-				'underline-color4': 'hsl(var(--underline-color4) / 0.2)',
-				'underline-color5': 'hsl(var(--underline-color5) / 0.2)',
-				'underline-color6': 'hsl(var(--underline-color6) / 0.2)',
-				'underline-color7': 'hsl(var(--underline-color7) / 0.2)',
-			},
-			fontFamily: {
-				inter: ['Inter', 'sans-serif'],
-			},
-		},
-	},
-	plugins: [],
+  darkMode: 'selector',
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      colors: {
+        'background-color': 'hsl(var(--background-color))',
+        'primary-color': 'hsl(var(--text-color))',
+        'secondary-color': 'hsl(var(--footer-color))',
+        'underline-color': 'hsl(var(--underline-color))',
+        'underline-hover-color': 'hsl(var(--footer-hover-color))',
+        'card-color': 'hsl(var(--card-color))',
+        'card-hover-color': 'hsl(var(--card-hover-color))',
+        'underline-color1': 'hsl(var(--underline-color1) / 0.2)',
+        'underline-color2': 'hsl(var(--underline-color2) / 0.2)',
+        'underline-color3': 'hsl(var(--underline-color3) / 0.2)',
+        'underline-color4': 'hsl(var(--underline-color4) / 0.2)',
+        'underline-color5': 'hsl(var(--underline-color5) / 0.2)',
+        'underline-color6': 'hsl(var(--underline-color6) / 0.2)',
+        'underline-color7': 'hsl(var(--underline-color7) / 0.2)',
+      },
+      fontFamily: {
+        inter: ['Inter', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
 }
